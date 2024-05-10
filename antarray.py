3eda86f6910af8eee8eb60cce52e89b78d279e71
Nikorasu <nikorasu85@gmail.com>
2024-05-09 21:07:21 -0500

    Overhauled/new 3d array version.

--- antarray.py
+++ antarray.py
@@ -1,103 +1,130 @@
 #!/usr/bin/env python3
 import numpy as np
 from time import sleep
+import os
 
 '''
 Rules for ant pheromone simulation within an array:
-- Main array can contain: empty-0, hive-1, food-2, walls-3, 10-17 (fooding ants), 20-27 (homing ants) (to indicate direction last moved)
-- Secondary array for pheromones: negative left by foodingAnts, positive by HomingAnts, both decreasing to 0 as they "evaporate"
-- as they move, ants leave "pheromone" values in the corresponding spot in the pheromone array, to indicate whatever it just came from, food or hive
-- ants can either be in fooding food mode (10-17), or bringing food home (20-27), and leave corresponding pheromones as they move
-- ants decide to move based on the spaces around them in the array, ignoring the spot they're on, and the spot "behind" the ant
-- each "round" ants move onto the space of their target pheromone, with the lowest value, if multiple pheromones present
-- ants cannot move into spaces already occupied by other ants, the hive, food, or walls.
-- when a foodingAnt finds food, it changes to homingAnt, and homingAnts change back to foodingAnts when they find hive
-- when target pheromones not detected, continue in same direction with occasional random variation +/- 1 (to avoid getting stuck in a corner)
-- if only the non-targeted pheromone is present (in front), move the towards strongest of that type, to hopefully follow similar-ants
-- when an ant moves onto a spot with an existing pheromone in the phero array, that value is added to the pheromone the ant would've left
+- Solid layer contains: empty-0, hive-1, food-2, walls-3, ants-10-17 (foodingAnt directions), 20-27 (homingAnt directions)
+- Pheromone layers are uint8: 0-255 for food pheromones and hive pheromones
+- Ants leave pheromones in the corresponding layer based on their mode (fooding or homing)
+- Ants decide to move based on the spaces around them, ignoring the spot they're on and the spot "behind" the ant
+- Each "round" ants move onto the space of their target pheromone with the lowest value, if multiple of those pheromones present
+- Ants cannot move into spaces already occupied by other ants, the hive, food, or walls
+- When a foodingAnt finds food, it changes to homingAnt, and homingAnts change back to foodingAnts when they find hive
+- When target pheromones not detected, continue in same direction with occasional random variation +/- 1 (to avoid getting stuck in a corner)
+- If only the non-targeted pheromone is present (in front), move towards strongest of that type, to hopefully follow similar-ants
+- When an ant moves onto a spot with an existing pheromone, that value will be added to the pheromone the ant will leave behind it
 '''
-arrows = ('🡑', '🡕', '🡒', '🡖', '🡓', '🡗', '🡐', '🡔') # for printing simulation state later, ants will be arrows indicating direction
-symbols = {0: ' ', 1: '\x1b[33m⭖\x1b[0m', 2: '\x1b[32m☘\x1b[0m', 3: '▒'} # for printing simulation state later
-directions = ((-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)) # up, up-right, right, down-right, down, down-left, left, up-left
-sim_size = (40,120)  # size of array - simulation space, fits in terminal
-p_lvl = 20  # initial strength-level of pheromones ants put out
+arrows = ('🡑', '🡕', '🡒', '🡖', '🡓', '🡗', '🡐', '🡔')  # for printing simulation state later, ants will be arrows indicating direction
+symbols = {1: '\x1b[31;1m⭖\x1b[0m', 2: '\x1b[32;1m☘\x1b[0m', 3: '▒'}  # empty, hive, food, wall
+directions = ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))  # up, up-right, right, down-right, down, down-left, left, up-left
+sim_size = (os.get_terminal_size().lines, os.get_terminal_size().columns)
+p_lvl = 50  # initial strength-level of pheromones ants put out
 
 class AntArray:
 
-    def __init__(self, size=sim_size, num_ants=10, num_food=1, ant_radius=7, food_radius=sim_size[1]//2):
-        self.array = np.zeros(size, dtype=np.int16)
-        self.phero = np.zeros(size, dtype=np.int16)
-        self.array[[0, -1], :] = self.array[:, [0, -1]] = 3  # place walls on edges of array
-        # place hive into middle of array
+    def __init__(self, size=(*sim_size,3), num_ants=20, num_food=1, ant_radius=10, food_radius=sim_size[1]//2):
+        # Initialize a 3D array
+        self.array = np.zeros(size, dtype=np.uint8)
+        # Place walls on edges of array on the first layer
+        self.array[[0, -1], :, 0] = self.array[:, [0, -1], 0] = 3
+        # Place hive into middle of array on the first layer
         hive_x, hive_y = size[0]//2, size[1]//4
-        self.array[hive_x, hive_y] = 1
+        self.array[hive_x, hive_y, 0] = 1
         # calculate the distance from each point to the hive
-        x_indices, y_indices = np.indices(size)
+        x_indices, y_indices = np.indices((size[0], size[1]))
         distances = np.sqrt((x_indices - hive_x)**2 + (y_indices - hive_y)**2)
         # place ants into array, randomly within ant_radius of the hive
-        a_indices = np.argwhere((self.array == 0) & (distances <= ant_radius))
+        a_indices = np.argwhere((self.array[:, :, 0] == 0) & (distances <= ant_radius))
         a_chosen = a_indices[np.random.choice(a_indices.shape[0], num_ants, replace=False)]
-        self.array[tuple(a_chosen.T)] = np.random.randint(10, 18, num_ants)
-        self.phero[tuple(a_chosen.T)] = -p_lvl  # each ant will leave pheromone in its spot in the phero array
+        self.array[a_chosen[:, 0], a_chosen[:, 1], 0] = np.random.randint(10, 18, num_ants)
+        #self.array[a_chosen[:, 0], a_chosen[:, 1], 1] = p_lvl
         # place food sources into array, randomly outside of food_radius from the hive
-        f_indices = np.argwhere((self.array == 0) & (distances > food_radius))
+        f_indices = np.argwhere((self.array[:, :, 0] == 0) & (distances > food_radius))
         f_chosen = f_indices[np.random.choice(f_indices.shape[0], num_food, replace=False)]
-        self.array[tuple(f_chosen.T)] = 2
-
+        self.array[f_chosen[:, 0], f_chosen[:, 1], 0] = 2
+    
     def print_state(self):
         output = ""
-        for x, row in enumerate(self.array):
+        for i, row in enumerate(self.array[:, :, 0]):  # iterate over rows of layer 0
             row_symbols = []
-            for y, value in enumerate(row.tolist()):
-                phero_value = self.phero[x][y]
-                if phero_value > 0:
-                    bg_color = f'\x1b[48;2;0;{int(phero_value)};0m' # green
-                elif phero_value < 0:
-                    bg_color = f'\x1b[48;2;{int(-phero_value)};0;0m' # red
-                else:
-                    bg_color = ''
-                if value in symbols:
-                    row_symbols.append(bg_color + symbols[value] + '\x1b[0m')
-                else:
-                    color_code = '\x1b[31m' if 10 <= value <= 17 else '\x1b[32m'
+            for j, value in enumerate(row):  # iterate over each value in the row
+                if value in symbols:  # symbol values represent the solid stuff, hive, food, walls, ants
+                    row_symbols.append(symbols[value])
+                elif 10 <= value < 28:
+                    color = '\x1b[31m' if 10 <= value <= 17 else '\x1b[32m' if 20 <= value <= 27 else ''
+                    bgcol = f'\x1b[48;2;{int(self.array[i, j, 1])};{int(self.array[i, j, 2])};0m'
                     arrow = arrows[value % 10]
-                    row_symbols.append(bg_color + color_code + arrow + '\x1b[0m')
+                    row_symbols.append(color + bgcol + arrow + '\x1b[0m')
+                else:
+                    # merge the pheromone values into a single RGB color code
+                    row_symbols.append(f'\x1b[48;2;{int(self.array[i, j, 1])};{int(self.array[i, j, 2])};0m \x1b[0m')
             output += ''.join(row_symbols) + "\n"
-        print(output)
-
+        print(output[:-1], end='\r')
+    
+    def scent_bubble(self, center, radius=10, layer=1):
+        center_y, center_x = center
+        y_dim, x_dim, _ = self.array.shape
+        for x in range(max(0, center_x-radius), min(x_dim, center_x+radius+1)):
+            for y in range(max(0, center_y-radius), min(y_dim, center_y+radius+1)):
+                dist = np.sqrt((x - center_x)**2 + (y - center_y)**2)  # Euclidean distance
+                if dist <= radius:
+                    value = int(dist)
+                    self.array[y, x, layer] = max(self.array[y, x, layer], value)
+    
     def update(self):
-        # Iterate over all cells with ants
-        ant_indices = np.argwhere((self.array >= 10) & (self.array <= 27))
+        for hive in np.argwhere(self.array[:, :, 0] == 1):
+            self.scent_bubble(hive, radius=15, layer=1)
+        for efood in np.argwhere(self.array[:, :, 0] == 2):
+            self.scent_bubble(efood, radius=10, layer=2)
+        ant_indices = np.argwhere((self.array[:, :, 0] >= 10) & (self.array[:, :, 0] <= 27))
         for x, y in ant_indices:
-            # Determine the ant's current state (fooding or homing) and direction
-            is_fooding = 10 <= self.array[x, y] <= 17
-            ant_direction = self.array[x, y] % 10
-            # get a list of values around ant, except the position directly behind current direction (direction+4)%8
-            #surrounds = [self.array[x + dx, y + dy] if (dx, dy) != directions[(ant_direction + 4) % 8] else 1003 for dx, dy in directions]
-            #surrounds = self.array[(x + np.array(directions)[:, 0]) % self.array.shape[0], (y + np.array(directions)[:, 1]) % self.array.shape[1]]
-            surrounds = self.array[x + np.array(directions)[:, 0], y + np.array(directions)[:, 1]].tolist()
-            surrounds[(ant_direction + 4) % 8] = 1003  # replace direction+4 with 1003 to ignore it
-            # one-third chance for the ant to turn left or right
-            ant_direction = (ant_direction + np.random.choice([-1, 0, 1], p=[1/6, 2/3, 1/6])) % 8
+            is_fooding = 10 <= self.array[x, y, 0] <= 17
+            ant_direction = self.array[x, y, 0] % 10
+            # Create a 3D surrounds array
+            surrounds = np.zeros((8, 3))
+            for i, (dx, dy) in enumerate(directions):
+                surrounds[i] = self.array[x + dx, y + dy]
+            # Create a 1D sees array
+            sees = np.zeros(8)
+            sees[0] = surrounds[ant_direction, 0]  # What's in front of the ant first
+            for offset in range(1, 4):  # Add elements with offsets of +/- 1, 2, 3 with wrap-around behavior
+                sees[2*offset-1] = surrounds[(ant_direction + offset)%8, 0]
+                sees[2*offset] = surrounds[(ant_direction - offset)%8, 0]
+            if is_fooding and 2 in surrounds[:, 0]: #if fooding and food present, change to homing
+                self.array[x, y, 0] = self.array[x, y, 0] + 10 #(self.array[x, y] % 10) + 20
+                is_fooding = False
+            elif not is_fooding and 1 in surrounds[:, 0]: #if homing and hive present, change to fooding
+                self.array[x, y, 0] = self.array[x, y, 0] - 10 #(self.array[x, y] % 10) + 10
+                is_fooding = True
+            # Check if 3 or more values in surround's first layer are walls, and if they are, randomly choose one of the directions which is 0
+            if np.sum(surrounds[:, 0] == 3) >= 3:
+                ant_direction = np.random.choice(np.where(surrounds[:, 0] == 0)[0])
+            elif is_fooding and any(0 < i <= 255 for i in surrounds[:, 2]): #follow food phero in direction originated
+                ant_direction = np.argmin(np.where(surrounds[:, 2] > 0, surrounds[:, 2], np.inf))
+            elif not is_fooding and any(0 < i <= 255 for i in surrounds[:, 1]): #follow hive phero in direction originated
+                ant_direction = np.argmin(np.where(surrounds[:, 1] > 0, surrounds[:, 1], np.inf))
+            else: # one-third chance for the ant to turn left or right
+                ant_direction = (ant_direction + np.random.choice([-1, 0, 1], p=[1/8, 3/4, 1/8])) % 8
             # Calculate the new position based on the ant's current direction
             nx, ny = np.add([x,y], directions[ant_direction])
             # Check if the new position is valid
-            if self.array[nx, ny] == 0:
+            if self.array[nx, ny, 0] == 0:
                 # Update the ant's position
-                self.array[x, y] = 0
-                self.array[nx, ny] = ant_direction + (10 if is_fooding else 20)
-                # Leave pheromones in the new position
-                self.phero[nx, ny] += -p_lvl if is_fooding else p_lvl
-                self.phero[nx, ny] = max(-255, min(255, self.phero[nx, ny]))
-
+                self.array[x, y, 0] = 0
+                self.array[nx, ny, 0] = ant_direction + (10 if is_fooding else 20)
+                # Add pheromones to the layer corresponding to the ant's state
+                self.array[x, y, 1 if is_fooding else 2] = min(255, self.array[x, y, 1 if is_fooding else 2] + p_lvl)
         # Evaporate pheromones
-        self.phero[self.phero > 0] -= 1
-        self.phero[self.phero < 0] += 1
+        mask = self.array[:, :, 1:3] > 0
+        self.array[:, :, 1:3][mask] -= 1
+
 
 if __name__ == '__main__':
     ant_array = AntArray()
     # Main simulation loop
     while True:
         ant_array.print_state()
-        sleep(0.2)
+        sleep(0.1)
         ant_array.update()
